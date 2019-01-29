37a8af2426d0f678ecc159f0972ea6f107f5f19a
petroselo <paultheskierguy@gmail.com>
2019-01-29 15:20:52 +0000

    Add IDP.ino again

---
+++ IDP.ino
@@ -0,0 +1,30 @@
+/*The Main file of the IDP project.
+   This will contain only the setup() and loop() functions. All else should be #included.
+   Any include will take up valuable space on the arduino so should be
+   modular and application specific.
+   I.E not including test-suite header files in final running build etc.
+   Arduino convention tab = 2 spaces
+   Each #include will also require it's initName(); function to be run in setup.
+*/
+//Add needed includes
+#include "Arduino.h"
+#include "move.h"
+#include "sense.h"
+#include "action.h"
+//#include "test.h"
+
+//Declare global variables
+
+void setup() {
+  initMove();
+  initSense();
+  initAction();
+  //initTest();
+
+}
+
+void loop() {
+  //make into a respond function for testing the switches in the test.cpp
+{
+
+}
